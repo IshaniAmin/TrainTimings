dd2b1a09869f133428f1ac3d50bbf26d7e6ad9b3
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-01-08 13:03:47 -0500

    added firebase link

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCaDSSE_wszpWiAof7MTSthbn89BtGR-K0",
+    authDomain: "traintimings-e4ea2.firebaseapp.com",
+    databaseURL: "https://traintimings-e4ea2.firebaseio.com",
+    storageBucket: "traintimings-e4ea2.appspot.com",
+    messagingSenderId: "305858823657"
+  };
+firebase.initializeApp(config);
+
+
+// Initialize Firebase variable
+var database = firebase.database();
+
+
+
